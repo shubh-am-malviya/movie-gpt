a8675ec7691ee32bed1a5b8ee2836fc51a6786b2
Shubham Malviya <you@example.com>
2023-09-28 16:13:51 +0530

    Main container style fix

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -13,7 +13,7 @@ const VideoBackground = ({ movieId }) => {
 			{trailerVideo && (
 				<iframe
 					className="w-screen aspect-video"
-					src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&controls=0&loop=1&rel=0`}
+					src={`https://www.youtube.com/embed/${trailerVideo?.key}?&autoplay=1&mute=1&controls=0&loop=1`}
 					title="YouTube video player"
 					allow="accelerometer; autoplay; modestbranding; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
 				></iframe>
--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const VideoInfo = ({ title, overview }) => {
 	return (
-		<div className="absolute w-screen aspect-video text-white pt-80 px-24 bg-gradient-to-tr from-black">
+		<div className="absolute w-screen aspect-video text-white pt-[20%] px-24 bg-gradient-to-r from-black to-transparent to-80%">
 			<h1 className="font-bold text-5xl">{title}</h1>
-			<p className="py-6 text-lg w-1/3">{overview}</p>
-			<button className="bg-white p-2 px-12 text-xl rounded-md text-black hover:bg-opacity-80">
+			<p className="py-6 text-lg w-1/4">{overview}</p>
+			<button className="bg-white p-2 px-12 text-xl rounded-md text-black cursor-pointer hover:bg-opacity-80">
 				▶️ Play
 			</button>
-			<button className="ml-2 bg-white p-2 px-12 text-xl rounded-md bg-opacity-30">
+			<button className="ml-2 bg-white p-2 px-12 text-xl rounded-md bg-opacity-30 cursor-pointer hover:bg-opacity-50">
 				More Info
 			</button>
 		</div>
